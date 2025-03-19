91ad7e2361791e28cb30dab18a129615cdcd4c6b
SaqibRazaDev <saqibraza.dev@gmail.com>
2025-03-19 22:10:26 +0500

    App Update

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div>
       <div className='h-screen flex flex-col justify-center items-center text-2xl md:text-5xl'>
         Welcome to Design In Studio
-        <p className="text-base">We are working on it.</p>
+        <p className="text-xs md:text-base md:my-3">We are working on it.</p>
       </div>
       
 

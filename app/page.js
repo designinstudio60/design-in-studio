c7c5716c7ff84622cefeb53b408ab3d3f607bd5c
SaqibRazaDev <saqibraza.dev@gmail.com>
2025-03-19 22:07:47 +0500

    App Upddate

--- app/page.js
+++ app/page.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div className='h-screen flex justify-center items-center text-2xl md:text-5xl'>Welcome to Design In Studio</div>
+    <div>
+      <div className='h-screen flex flex-col justify-center items-center text-2xl md:text-5xl'>
+        Welcome to Design In Studio
+        <p className="text-base">We are working on it.</p>
+      </div>
+      
+
+
+    </div>
   )
 }

f281ef09b0ed8fddb7a07290cbd740054d6dc741
ramzanzubair01 <ramzanzubair01@gmail.com>
2025-04-11 15:04:00 +0500

    favicon update

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,33 @@
+// import { Geist, Geist_Mono } from "next/font/google";
+// import "./globals.css";
+// import Head from 'next/head';
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+// export const metadata = {
+//   title: "Design In Studio | You ultimate mixkit",
+//   description: "Developed by Saqib Raza",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -12,16 +42,19 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Design In Studio | You ultimate mixkit",
+  title: "Design In Studio | Your ultimate mixkit",
   description: "Developed by Saqib Raza",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <link rel="icon" href="/favicon.svg" />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>

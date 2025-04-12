700b13fecee3ccd996ec32db6fe7e67db1187acb
ramzanzubair01 <ramzanzubair01@gmail.com>
2025-04-12 16:40:51 +0500

    portfolio community

--- app/page.js
+++ app/page.js
@@ -7,32 +7,10 @@ import PortfolioCommunity from '@/components/PortfolioCommunity';
 export default function Home() {
   return (
     <div className="min-h-screen">
-      {/* Full width container */}
-      
-      <div className="container">
-        {/* Section with custom width */}
-        
-        {/* <section className="section">
-        <Navbar />
-         <HeroSection />
-         <PortfolioCommunity />
-
-        
-
-          
-        </section> */}
-        <section className="section">
-        <Navbar />
-        <HeroSection />
-        </section>
-        
-        <ServicesSection />
-        <section className="section">
-        <PortfolioCommunity />
-        </section>
-        
-
-      </div>
+      <Navbar />
+      <HeroSection />
+      <ServicesSection />
+      <PortfolioCommunity />
       
       
     </div>
